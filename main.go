1762fe3105509a435f9ad4901de5ef047192bb62
Ivan Sharovarov <sharovarov31@gmail.com>
2020-07-04 17:36:22 +0600

    set url and GET response body

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	urlFlag := flag.String("url", "https://gophercises.com", "the url you want to build a sitemap for")
+	flag.Parse()
+
+	resp, err := http.Get(*urlFlag)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	io.Copy(os.Stdout, resp.Body)
+}
