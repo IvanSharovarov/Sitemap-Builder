ebfb505e8b4184151101c3cf016f6b941e85dbc5
Ivan Sharovarov <sharovarov31@gmail.com>
2020-07-04 19:22:31 +0600

    parsing and building links

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"flag"
-	"io"
+	"fmt"
+	html_link_parser "github.com/IvanSharovarov/html-link-parser"
 	"net/http"
-	"os"
+	"net/url"
+	"strings"
 )
 
 func main() {
@@ -17,5 +19,29 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	io.Copy(os.Stdout, resp.Body)
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
+
+
+	links, err := html_link_parser.Parse(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var hrefs []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base + l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		}
+	}
+
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
 }
